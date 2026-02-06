9adcb84e3c28c30143bd565588c7dbe30e639b8d
Lareina Liu <LareinaCornia0808@163.com>
2026-02-06 17:07:04 -0500

    feat: M2E1 add support for RPC

--- distribution/util/wire.js
+++ distribution/util/wire.js
@@ -5,13 +5,37 @@
  * @typedef {import("../types.js").Hasher} Hasher
  */
 const log = require('../util/log.js');
+const id = require('../util/id.js');
 
+const toLocal = {};
 
 /**
  * @param {Function} func
  */
 function createRPC(func) {
   // Write some code...
+  const remotePointer = id.getID(func.toString() + Math.random());
+  toLocal[remotePointer] = func;
+
+  const currentNode = typeof globalThis.local !== 'undefined' ? globalThis.node : 'UNKNOWN_NODE';
+
+  const stub = (...args) => {
+    const cb = args.pop();
+    const remote = { 
+      node: "__NODE_INFO__", 
+      service: 'rpc', 
+      method: "__RPC_ID__" 
+    };
+  
+    globalThis.local.comm.send(args, remote, cb);
+  };
+
+  let stubStr = stub.toString()
+    .replace("__NODE_INFO__", currentNode)
+    .replace("__RPC_ID__", remotePointer);
+
+  const finalStub = new Function(`return ${stubStr}`)();
+  return finalStub;
 }
 
 /**
@@ -38,8 +62,20 @@ function toAsync(func) {
   return asyncFunc;
 }
 
+function rpcHandler(args, remote, cb) {
+  const pointer = remote.method;
+  const func = toLocal[pointer];
+  
+  if (typeof func === 'function') {
+    func(...args, cb);
+  } else {
+    cb(new Error(`RPC Method ${pointer} not found on this node.`));
+  }
+}
 
 module.exports = {
   createRPC,
   toAsync,
+  toLocal,
+  rpcHandler,
 };
