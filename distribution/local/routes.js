c0d54ceedf0ce76c5cb808fd23ad99076610c4c7
Lareina Liu <LareinaCornia0808@163.com>
2026-02-08 15:05:19 -0500

    fix: RPC bug

--- distribution/local/routes.js
+++ distribution/local/routes.js
@@ -44,18 +44,20 @@ function put(service, configuration, callback) {
     let finalService = s;
 
     if (typeof s === 'object' && s !== null) {
+      const methods = Object.keys(s);
       const hasRPC = Object.values(s).some(f => typeof f === 'function' && f.__is_rpc_stub__);
       
       if (hasRPC) {
-        finalService = Object.assign(Object.create(Object.getPrototypeOf(s)), s);
-        
-        for (const [method, fn] of Object.entries(s)) {
-          if (typeof fn === 'function' && fn.__is_rpc_stub__) {
-            let src = fn.toString();
-            src = src.replace('="__NODE_INFO__"', `=${JSON.stringify(globalThis.distribution.node.config)}`);
-            src = src.replace('="__RPC_PTR__"', `="${fn.__rpc_ptr__}"`);
+        finalService = { ...s };
+        for (const m of methods) {
+          if (typeof s[m] === 'function' && s[m].__is_rpc_stub__) {
+            let src = s[m].toString();
             
-            finalService[method] = eval(`(${src})`);
+            src = src.replace('="__NODE_INFO__"', `=${JSON.stringify(globalThis.distribution.node.config)}`);
+            src = src.replace('="__RPC_PTR__"', `="${s[m].__rpc_ptr__}"`);
+            
+            finalService[m] = eval(`(${src})`);
+            finalService[m].__is_rpc_stub__ = true;
           }
         }
       }
