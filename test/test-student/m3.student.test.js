ad5a066777db909cc0ae575ebf9ecc89d6795e4a
Lareina Liu <LareinaCornia0808@163.com>
2026-02-21 13:02:27 -0500

    feat: add M3 student test

--- test/test-student/m3.student.test.js
+++ test/test-student/m3.student.test.js
@@ -9,29 +9,166 @@
 const distribution = require('../../distribution.js')();
 require('../helpers/sync-guard');
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+const id = distribution.util.id;
+
+jest.spyOn(process, 'exit').mockImplementation(() => {});
+
+const mygroupGroup = {};
+
+const n1 = {ip: '127.0.0.1', port: 9100};
+const n2 = {ip: '127.0.0.1', port: 9101};
+const n3 = {ip: '127.0.0.1', port: 9102};
+
+test('(1 pts) local.groups.put/get()', (done) => {
+  const g = {
+    [id.getSID(n1)]: n1,
+    [id.getSID(n2)]: n2,
+  };
+
+  distribution.local.groups.put('student', g, (e) => {
+    if (e) return done(e);
+    distribution.local.groups.get('student', (e, v) => {
+      try {
+        expect(e).toBeFalsy();
+        expect(v).toEqual(g);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
 });
 
+test('(1 pts) all.status.get(nid)', (done) => {
+  const expected = Object.values(mygroupGroup).map(n => id.getNID(n));
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  distribution.mygroup.status.get('nid', (e, v) => {
+    try {
+      expect(e).toEqual({});
+      expect(Object.values(v)).toEqual(expect.arrayContaining(expected));
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
 });
 
+test('(1 pts) all.comm.send(status.get(nid))', (done) => {
+  const expected = Object.values(mygroupGroup).map(n => id.getNID(n));
+  const remote = {service: 'status', method: 'get'};
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+  distribution.mygroup.comm.send(['nid'], remote, (e, v) => {
+    try {
+      expect(e).toEqual({});
+      expect(Object.values(v)).toEqual(expect.arrayContaining(expected));
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
 });
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+test('(1 pts) all.routes.put()', (done) => {
+  const svc = {
+    ping: () => 'pong',
+  };
+
+  distribution.mygroup.routes.put(svc, 'ping', (e) => {
+    if (e && Object.keys(e).length > 0) return done(e);
+
+    const remote = {node: n1, service: 'routes', method: 'get'};
+    distribution.local.comm.send(['ping'], remote, (e, v) => {
+      try {
+        expect(e).toBeFalsy();
+        expect(v.ping()).toEqual('pong');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
 });
 
-test('(1 pts) student test', (done) => {
-  // Fill out this test case...
-    done(new Error('Not implemented'));
+test('(1 pts) all.groups.put/get()', (done) => {
+  const g = {
+    foo: {ip: '127.0.0.1', port: 9999},
+  };
+
+  distribution.mygroup.groups.put('xgroup', g, (e) => {
+    if (e && Object.keys(e).length > 0) return done(e);
+
+    distribution.mygroup.groups.get('xgroup', (e, v) => {
+      try {
+        expect(e).toEqual({});
+        Object.values(v).forEach(view => {
+          expect(view).toEqual(g);
+        });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+});
+
+beforeAll((done) => {
+  const remote = {service: 'status', method: 'stop'};
+
+  remote.node = n1;
+  distribution.local.comm.send([], remote, () => {
+    remote.node = n2;
+    distribution.local.comm.send([], remote, () => {
+      remote.node = n3;
+      distribution.local.comm.send([], remote, start);
+    });
+  });
+
+  function start() {
+    mygroupGroup[id.getSID(n1)] = n1;
+    mygroupGroup[id.getSID(n2)] = n2;
+    mygroupGroup[id.getSID(n3)] = n3;
+
+    distribution.node.start((e) => {
+      if (e) return done(e);
+
+      distribution.local.status.spawn(n1, (e) => {
+        if (e) return done(e);
+        distribution.local.status.spawn(n2, (e) => {
+          if (e) return done(e);
+          distribution.local.status.spawn(n3, (e) => {
+            if (e) return done(e);
+
+            distribution.local.groups.put(
+              {gid: 'mygroup'},
+              mygroupGroup,
+              (e) => {
+                if (e) return done(e);
+                done();
+              }
+            );
+          });
+        });
+      });
+    });
+  }
+});
+
+afterAll((done) => {
+  distribution.mygroup.status.stop(() => {
+    const remote = {service: 'status', method: 'stop'};
+
+    remote.node = n1;
+    distribution.local.comm.send([], remote, () => {
+      remote.node = n2;
+      distribution.local.comm.send([], remote, () => {
+        remote.node = n3;
+        distribution.local.comm.send([], remote, () => {
+          if (globalThis.distribution.node.server) {
+            globalThis.distribution.node.server.close();
+          }
+          done();
+        });
+      });
+    });
+  });
 });
