ab953d4f74690da51b8e1f3e553f72a6cdc304a1
davideskilson <david_eskilson@brown.edu>
2026-04-20 13:13:10 +0000

    feat: bench simple + complex

--- distribution.js
+++ distribution.js
@@ -9,11 +9,11 @@ const log = require('./distribution/util/log.js');
  * @param {Node} [config]
  */
 function bootstrap(config) {
-    // This loads the reference implementation in case you want to selectively override parts of your implementation with it (e.g., extra credit ypu are not doing or missing functionality from previous milestones).
-    // This needs to run before the rest of the code so that the globalThis.distribution object is populated correctly.
-    // @ts-ignore Optional dependency for reference implementation.
-    const distributionLib = require('@brown-ds/distribution')(config);
-    distributionLib; // To avoid unused variable warning
+  // This loads the reference implementation in case you want to selectively override parts of your implementation with it (e.g., extra credit ypu are not doing or missing functionality from previous milestones).
+  // This needs to run before the rest of the code so that the globalThis.distribution object is populated correctly.
+  // @ts-ignore Optional dependency for reference implementation.
+  const distributionLib = require('@brown-ds/distribution')(config);
+  distributionLib; // To avoid unused variable warning
   const distribution = {};
 
   // @ts-ignore This is the first time globalThis.distribution is being initialized, so the object does not have all the necessary properties.
@@ -27,8 +27,8 @@ function bootstrap(config) {
   }
   distribution.local = require('./distribution/local/local.js');
 
-  const {setup} = require('./distribution/all/all.js');
-  distribution.all = setup({gid: 'all'});
+  const { setup } = require('./distribution/all/all.js');
+  distribution.all = setup({ gid: 'all' });
 
   /* Overrides when missing functionality from previous milestone or extra credit is needed */
 
@@ -42,7 +42,7 @@ function bootstrap(config) {
   // __end_M3_solution__ */
 
   for (const [key, service] of Object.entries(distribution.local)) {
-    distribution.local.routes.put(service, key, () => {});
+    distribution.local.routes.put(service, key, () => { });
   }
 
   return distribution;
@@ -56,7 +56,7 @@ function bootstrap(config) {
   Set "useLibrary" in package.json to true or false accordingly.
 */
 // @ts-ignore JSON import resolved at runtime.
-const {useLibrary} = require('./package.json');
+const { useLibrary } = require('./package.json');
 // @ts-ignore Optional dependency for reference implementation.
 const distribution = useLibrary ? require('@brown-ds/distribution') : bootstrap;
 
